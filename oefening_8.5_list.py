c0f6a9881f0dec736bac7e613304082953c119b7
Toon Meynen <toon.meynen@cinqict.nl>
2014-11-22 18:38:29 +0100

    Rewrite code

--- oefening_8.5_list.py
+++ oefening_8.5_list.py
@@ -1,20 +1,20 @@
 __author__ = 'Toon Meynen'
 
-fh = open('mbox-short.txt')
+fname = raw_input("Enter file name: ")
+if len(fname) < 1:
+    fname = 'mbox-short.txt'
+fh = open(fname)
 
+count = 0
 emails = list()
 
 for line in fh:
     line.rstrip()
     if not line.startswith('From'):
         continue
-    chopped = line.split()
-    emails.append(chopped[1])
-
-for email in emails:
-    print email
-
-count = len(emails)
+    words = line.split()
+    if words[0] == 'From':
+        print words[1]
+        count += 1
 
 print "There were", count, "lines in the file with From as the first word"
-
